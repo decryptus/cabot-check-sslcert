4d56dc3886108598cb02ae532660744a3536ffbc
Adrien Delle Cave <adrien.delle.cave@commandersact.com>
2018-03-22 16:48:28 +0100

    Fixed setup.py details

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import find_packages, setup
 
 setup(name='cabot_check_sslcert',
-      version='0.1.1',
+      version='0.1.2',
       description='A SSL certificat check plugin for Cabot',
       author='Adrien DELLE CAVE',
-      author_email='adc@doowan.net',
-      url='https://github.com/decryptus/',
+      author_email='pypi@doowan.net',
+      url='https://github.com/decryptus/cabot-check-sslcert',
       packages=find_packages(),
     )
